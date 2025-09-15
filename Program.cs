c266206483a4b68ed524d296cc975ad140250d69
Lorenz Alfred Jose Lauraya <lorenz.lauraya@inchcape.com>
2025-09-15 14:28:16 +0800

    Changed settings

--- Program.cs
+++ Program.cs
@@ -1,14 +1,5 @@
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-var dbServer = Environment.GetEnvironmentVariable("DB_SERVER");
-var dbName = Environment.GetEnvironmentVariable("DB_NAME");
-var dbUser = Environment.GetEnvironmentVariable("DB_USER");
-var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
-
-var connectionString = $"Server={dbServer};Database={dbName};User Id={dbUser};Password={dbPassword};TrustServerCertificate=True;";
-
-builder.Configuration["ConnectionStrings:umbracoDbDSN"] = connectionString;
-
 builder.CreateUmbracoBuilder()
     .AddBackOffice()
     .AddWebsite()
